113b0b86404d528a0b1f554e4569c9c859e78a53
Kat <kat.zien@brightpearl.com>
2016-12-07 01:31:58 +0000

    Day 1 part 2 solution

--- 2016/1/2/main.go
+++ 2016/1/2/main.go
@@ -12,12 +12,77 @@ var verbose = false
 func main() {
 	input := "L4, L1, R4, R1, R1, L3, R5, L5, L2, L3, R2, R1, L4, R5, R4, L2, R1, R3, L5, R1, L3, L2, R5, L4, L5, R1, R2, L1, R5, L3, R2, R2, L1, R5, R2, L1, L1, R2, L1, R1, L2, L2, R4, R3, R2, L3, L188, L3, R2, R54, R1, R1, L2, L4, L3, L2, R3, L1, L1, R3, R5, L1, R5, L1, L1, R2, R4, R4, L5, L4, L1, R2, R4, R5, L2, L3, R5, L5, R1, R5, L2, R4, L2, L1, R4, R3, R4, L4, R3, L4, R78, R2, L3, R188, R2, R3, L2, R2, R3, R1, R5, R1, L1, L1, R4, R2, R1, R5, L1, R4, L4, R2, R5, L2, L5, R4, L3, L2, R1, R1, L5, L4, R1, L5, L1, L5, L1, L4, L3, L5, R4, R5, R2, L5, R5, R5, R4, R2, L1, L2, R3, R5, R5, R5, L2, L1, R4, R3, R1, L4, L2, L3, R2, L3, L5, L2, L2, L1, L2, R5, L2, L2, L3, L1, R1, L4, R2, L4, R3, R5, R3, R4, R1, R5, L3, L5, L5, L3, L2, L1, R3, L4, R3, R2, L1, R3, R1, L2, R4, L3, L3, L3, L1, L2"
 
-	log.Printf("The first location visited twice is %d blocks away.", distanceToFirstRevisitedLocation(input))
+	if d := distanceToFirstRevisitedLocation(input); d > 0 {
+		log.Printf("The first location visited twice is %d blocks away.", distanceToFirstRevisitedLocation(input))
+	} else {
+		log.Println("It looks like we didn't visit any location twice.")
+	}
+
 }
 
-type move struct {
-	direction string
-	distance  int
+func distanceToFirstRevisitedLocation(input string) int {
+	direction := "N"
+	location := position{x: 0, y: 0}
+
+	moves := parseInput(input)
+
+	list := locations{visited: make(map[position]struct{}, 0)}
+
+	if verbose {
+		log.Printf("Moves: %v", moves)
+	}
+
+	for _, move := range moves {
+
+		if verbose {
+			log.Printf("Currently at %v", location)
+			log.Printf("Next move: %v", move)
+		}
+
+		if direction == "N" && move.direction == "L" || direction == "S" && move.direction == "R" {
+			direction = "W"
+			for i := 0; i < move.distance; i++ {
+				location.x--
+				if list.seen(location) {
+					return distance(location)
+				}
+				list.add(location)
+			}
+		} else if direction == "N" && move.direction == "R" || direction == "S" && move.direction == "L" {
+			direction = "E"
+			for i := 0; i < move.distance; i++ {
+				location.x++
+				if list.seen(location) {
+					return distance(location)
+				}
+				list.add(location)
+			}
+		} else if direction == "E" && move.direction == "L" || direction == "W" && move.direction == "R" {
+			direction = "N"
+			for i := 0; i < move.distance; i++ {
+				location.y++
+				if list.seen(location) {
+					return distance(location)
+				}
+				list.add(location)
+			}
+		} else if direction == "W" && move.direction == "L" || direction == "E" && move.direction == "R" {
+			direction = "S"
+			for i := 0; i < move.distance; i++ {
+				location.y--
+				if list.seen(location) {
+					return distance(location)
+				}
+				list.add(location)
+			}
+		}
+	}
+
+	if verbose {
+		log.Printf("Finished at [%d, %d] facing %s", location.x, location.y, direction)
+	}
+
+	return 0
 }
 
 func parseInput(input string) []move {
@@ -41,67 +106,32 @@ func parseInput(input string) []move {
 	return moves
 }
 
-func distanceToFirstRevisitedLocation(input string) int {
+func distance(location position) int {
+	return int(math.Abs(float64(location.x)) + math.Abs(float64(location.y)))
+}
 
-	var direction = "N"
-	var x, y int
+type move struct {
+	direction string
+	distance  int
+}
 
-	moves := parseInput(input)
+type position struct {
+	x, y int
+}
 
+type locations struct {
+	visited map[position]struct{}
+}
+
+func (l *locations) add(place position) {
+	l.visited[place] = struct{}{}
+}
+
+func (l *locations) seen(place position) bool {
 	if verbose {
-		log.Printf("Moves: %v", moves)
+		log.Printf("Checking if we've been to %v already", place)
 	}
 
-	for _, move := range moves {
-
-		if verbose {
-			log.Printf("Currently at [%d, %d] facing %s", x, y, direction)
-			log.Printf("Next move: %v", move)
-		}
-
-		switch direction {
-		case "N":
-			switch move.direction {
-			case "L":
-				direction = "W"
-				x = x - move.distance
-			case "R":
-				direction = "E"
-				x = x + move.distance
-			}
-		case "S":
-			switch move.direction {
-			case "L":
-				direction = "E"
-				x = x + move.distance
-			case "R":
-				direction = "W"
-				x = x - move.distance
-			}
-		case "W":
-			switch move.direction {
-			case "L":
-				direction = "S"
-				y = y - move.distance
-			case "R":
-				direction = "N"
-				y = y + move.distance
-			}
-		case "E":
-			switch move.direction {
-			case "L":
-				direction = "N"
-				y = y + move.distance
-			case "R":
-				direction = "S"
-				y = y - move.distance
-			}
-		}
-	}
-
-	if verbose {
-		log.Printf("Finished at [%d, %d] facing %s", x, y, direction)
-	}
-
-	return int(math.Abs(float64(x)) + math.Abs(float64(y)))
+	_, exists := l.visited[place]
+	return exists
 }
--- 2016/1/2/main_test.go
+++ 2016/1/2/main_test.go
@@ -7,14 +7,14 @@ import (
 )
 
 func TestExample1(t *testing.T) {
-	assert.Equal(t, 5, distanceToFirstRevisitedLocation("R2, L3"))
+	assert.Equal(t, 4, distanceToFirstRevisitedLocation("R8, R4, R4, R8"))
 }
 
-// func TestFinalAnswer(t *testing.T) {
-// 	input := "L4, L1, R4, R1, R1, L3, R5, L5, L2, L3, R2, R1, L4, R5, R4, L2, R1, R3, L5, R1, L3, L2, R5, L4, L5, R1, R2, L1, R5, L3, R2, R2, L1, R5, R2, L1, L1, R2, L1, R1, L2, L2, R4, R3, R2, L3, L188, L3, R2, R54, R1, R1, L2, L4, L3, L2, R3, L1, L1, R3, R5, L1, R5, L1, L1, R2, R4, R4, L5, L4, L1, R2, R4, R5, L2, L3, R5, L5, R1, R5, L2, R4, L2, L1, R4, R3, R4, L4, R3, L4, R78, R2, L3, R188, R2, R3, L2, R2, R3, R1, R5, R1, L1, L1, R4, R2, R1, R5, L1, R4, L4, R2, R5, L2, L5, R4, L3, L2, R1, R1, L5, L4, R1, L5, L1, L5, L1, L4, L3, L5, R4, R5, R2, L5, R5, R5, R4, R2, L1, L2, R3, R5, R5, R5, L2, L1, R4, R3, R1, L4, L2, L3, R2, L3, L5, L2, L2, L1, L2, R5, L2, L2, L3, L1, R1, L4, R2, L4, R3, R5, R3, R4, R1, R5, L3, L5, L5, L3, L2, L1, R3, L4, R3, R2, L1, R3, R1, L2, R4, L3, L3, L3, L1, L2"
+func TestFinalAnswer(t *testing.T) {
+	input := "L4, L1, R4, R1, R1, L3, R5, L5, L2, L3, R2, R1, L4, R5, R4, L2, R1, R3, L5, R1, L3, L2, R5, L4, L5, R1, R2, L1, R5, L3, R2, R2, L1, R5, R2, L1, L1, R2, L1, R1, L2, L2, R4, R3, R2, L3, L188, L3, R2, R54, R1, R1, L2, L4, L3, L2, R3, L1, L1, R3, R5, L1, R5, L1, L1, R2, R4, R4, L5, L4, L1, R2, R4, R5, L2, L3, R5, L5, R1, R5, L2, R4, L2, L1, R4, R3, R4, L4, R3, L4, R78, R2, L3, R188, R2, R3, L2, R2, R3, R1, R5, R1, L1, L1, R4, R2, R1, R5, L1, R4, L4, R2, R5, L2, L5, R4, L3, L2, R1, R1, L5, L4, R1, L5, L1, L5, L1, L4, L3, L5, R4, R5, R2, L5, R5, R5, R4, R2, L1, L2, R3, R5, R5, R5, L2, L1, R4, R3, R1, L4, L2, L3, R2, L3, L5, L2, L2, L1, L2, R5, L2, L2, L3, L1, R1, L4, R2, L4, R3, R5, R3, R4, R1, R5, L3, L5, L5, L3, L2, L1, R3, L4, R3, R2, L1, R3, R1, L2, R4, L3, L3, L3, L1, L2"
 
-// 	assert.Equal(t, 279, distanceToFirstRevisitedLocation(input))
-// }
+	assert.Equal(t, 163, distanceToFirstRevisitedLocation(input))
+}
 
 func TestParseInput(t *testing.T) {
 	expected := []move{
