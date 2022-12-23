f85cbe8fd5b689fed296b85bc7e1cb0b5e71d4b4
katzien <kasia.zien@gmail.com>
2022-12-24 00:43:09 +0100

    Day 4 part 2 done

--- 2022/day4/part2.go
+++ 2022/day4/part2.go
@@ -1,13 +1,38 @@
 package day4
 
 import (
+	"fmt"
 	"os"
 )
 
 func Part2() {
-	_, err := os.ReadFile("./day4/input")
+	list, err := os.ReadFile("./day4/input")
 	if err != nil {
 		panic(err)
 	}
 
+	input := parseInput(string(list))
+
+	count := findAnyOverlaps(input)
+
+	fmt.Printf("There are %d overlapping pairs\n", count)
+}
+
+func findAnyOverlaps(pairs []Pair) int {
+	count := 0
+	for _, p := range pairs {
+		// the only time there's no overlap is if either the first section finishes before the second,
+		// or the second finishes before the first - e.g.
+		// .23456...  2-6
+		// ......78.  4-8
+
+		// ......78.  4-8
+		// .23456...  2-6
+		if p.Elf1.End < p.Elf2.Start || p.Elf1.Start > p.Elf2.End {
+			continue
+		}
+		count++
+	}
+
+	return count
 }
--- 2022/main.go
+++ 2022/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	day4.Part1()
+	day4.Part2()
 }
